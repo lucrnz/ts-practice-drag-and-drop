5796f2f229f7ac8352278f5c0a936459d9967121
lucdev <me@lucdev.net>
2023-02-20 12:17:39 -0300

    Add drag and drop note

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 // Drag & Drop Interfaces
+// Read more about Drag on Drop:
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
 interface Draggable {
   dragStartHandler(event: DragEvent): void;
   dragEndHandler(event: DragEvent): void;
